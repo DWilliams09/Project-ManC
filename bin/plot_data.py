2e445f14bde7f45334df33fe625027781c61e6b6
DWilliams09 <35878205+DWilliams09@users.noreply.github.com>
2018-01-28 14:35:18 -0800

    Update plot_data.py

--- bin/plot_data.py
+++ bin/plot_data.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python3
+###
+# Name: Tim Frenzel
+# Student ID: 002267120
+# Email: frenz102@mail.chapman.edu
+# Course: CS510 Fall 2017
+###
 
+#######  START: CREATE PLOT #######
+#import packages
 import sys
 import numpy as np
 import pandas as pd
@@ -10,48 +17,43 @@ import matplotlib.pyplot as plt
 def main():
 	file_dir = sys.argv[1]
 
-	d = pd.read_csv(file_dir, header=None)
-	N = len(d) ## Total number of cells in our matrix
+	data = pd.read_csv(file_dir, header=None)
+	N = len(data)
+	
+	#determine axis
+	xmin = data.ix[0,0]
+	ymin = data.ix[0,1]
+	xmax = data.ix[len(data)-1,0]
+	ymax = data.ix[len(data)-1,1]
+	ystep = data.ix[1,1] - data.ix[0,1]
+		ylen = int(round((ymax-ymin)/ystep))+1
+		xlen = int(N/ylen)
 
-	xmin = d.ix[0,0]
-	ymin = d.ix[0,1]
-	xmax = d.ix[len(d)-1,0]
-	ymax = d.ix[len(d)-1,1]
-
-	ystep = d.ix[1,1] - d.ix[0,1]
-
-	ylen = int(round((ymax-ymin)/ystep))+1
-	xlen = int(N/ylen)
-
-	d = d.ix[:,2]
-	d = pd.DataFrame(d.values.reshape([xlen,ylen]))
-	d = d.transpose()
-
-	plt.matshow(d, cmap='jet')
-
+	data = data.ix[:,2]
+	data = pd.DataFrame(data.values.reshape([xlen,ylen]))
+	data = data.transpose()
+	
+	#show plot
+	plt.matshow(data, cmap='jet')
 	ax = plt.gca()
-	### Format axis ticks and labels
-	ax.invert_yaxis()
-	ax.xaxis.set_ticks_position('bottom') 
-	xlabels = [str(round(f,6)) for f in np.linspace(xmin, xmax, 5)]
-	ylabels = [str(round(f,6)) for f in np.linspace(ymin, ymax, 5)]
+	
+	#axis ticks 
+		ax.invert_yaxis()
+		ax.xaxis.set_ticks_position('bottom') 
+		xlabels = [str(round(f,6)) for f in np.linspace(xmin, xmax, 5)]
+		ylabels = [str(round(f,6)) for f in np.linspace(ymin, ymax, 5)]
 
-	ax.xaxis.set_ticks(np.linspace(0, xlen, 5))
-	ax.yaxis.set_ticks(np.linspace(0, ylen, 5))
-	ax.xaxis.set_ticklabels(xlabels)
-	ax.yaxis.set_ticklabels(ylabels)
-	### Set axis labels
-	ax.xaxis.set_label_position('bottom')
-	plt.xlabel(r"$c$", size=12)
-	plt.ylabel(r"$z$", size=12)
-
+		ax.xaxis.set_ticks(np.linspace(0, xlen, 5))
+		ax.yaxis.set_ticks(np.linspace(0, ylen, 5))
+		ax.xaxis.set_ticklabels(xlabels)
+		ax.yaxis.set_ticklabels(ylabels)
+	
+	#save file
 	fname = file_dir.split('/')[-1]
 	fname = fname.split('.')[0]
 	png = 'data/{}.png'.format(fname)
 	plt.savefig(png)
-	print("Wrote", png)
 
-	plt.show()
-
+#######  END: CREATE PLOT #######
 if __name__ == "__main__":
 	main()
