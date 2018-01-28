922a73381fb32d7c37ad0566dfb8d991ccbda657
DWilliams09 <35878205+DWilliams09@users.noreply.github.com>
2018-01-28 13:44:48 -0800

    Update generate_more.sh

--- bin/generate_more.sh
+++ bin/generate_more.sh
@@ -6,14 +6,14 @@ make || exit
 
 mkdir -p data || exit
 
-bin/julia -2 2 -2 2 800 800 -2.1 1.25 >data/julia2.csv
+bin/julia -2 2 -2 2 800 800 -1.476 0 >data/julia2.csv
 bin/plot_data.py data/julia2.csv
 
-bin/julia -2 2 -2 2 800 800 -1.1 0.37 >data/julia3.csv
+bin/julia -2 2 -2 2 800 800 0.3 -0.1 >data/julia3.csv
 bin/plot_data.py data/julia3.csv
 
 bin/mandelbrot -2 2 -2 2 800 800 0.5 0.5 >data/mandelbrot2.csv
 bin/plot_data.py data/mandelbrot2.csv
 
-bin/mandelbrot -2 2 -2 2 800 800 -0.6 -0.1 >data/mandelbrot3.csv
+bin/mandelbrot -2 2 -2 2 800 800 -0.8 0.2 >data/mandelbrot3.csv
 bin/plot_data.py data/mandelbrot3.csv
