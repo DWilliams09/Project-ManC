0bef3def2b74171405bbdcc8d73c26716131bfd4
DWilliams09 <35878205+DWilliams09@users.noreply.github.com>
2018-01-28 14:55:03 -0800

    Update complex.c

--- src/julia/complex.c
+++ src/julia/complex.c
@@ -1,30 +1,39 @@
+/*###
+# Name: Tim Frenzel
+# Student ID: 002267120
+# Email: frenz102@mail.chapman.edu
+# Course: CS510 Fall 2017
+###
+
+####### START: COMPLEX FUNCTIONS #######
+#import packages */
 #include <stdio.h>
 #include <math.h>
 #include "complex.h"
 
-COMPLEX mult_complex(COMPLEX a, COMPLEX b) {
-    COMPLEX z;
-    z.x = a.x * b.x - a.y * b.y;
-    z.y = a.x * b.y + a.y * b.x;
-    return z;
-}
-
-COMPLEX square_complex(COMPLEX a) {
-    return mult_complex(a, a);
-}
-
-COMPLEX add_complex(COMPLEX a, COMPLEX b) {
-    COMPLEX z;
-    z.x = a.x + b.x;
-    z.y = a.y + b.y;
-    return z;
-}
-
-long double abs_complex(COMPLEX a) {
-    long double rsq = a.x * a.x + a.y * a.y;
+long double complex_abs(COMPLEX a) {
+        rsq = a.x * a.x + a.y * a.y;
+        rsq = lround(rsq)
     return sqrtl(rsq);
 }
 
-void print_complex(COMPLEX a){
-    printf("%Lf + %LF i", a.x, a.y);
+COMPLEX complex_add(COMPLEX a, COMPLEX b) {
+    COMPLEX z;
+        z.x = a.x + b.x;
+        z.y = a.y + b.y;
+    return z;
 }
+
+COMPLEX complex_multi(COMPLEX a, COMPLEX b) {
+    COMPLEX z;
+        z.x = a.x * b.x - a.y * b.y;
+        z.y = a.x * b.y + a.y * b.x;
+    return z;
+}
+
+COMPLEX complex_2(COMPLEX a) {
+        z = complex_multi(a, a);
+    return z;
+}
+
+*/ ####### END: COMPLEX FUNCTIONS ####### */
